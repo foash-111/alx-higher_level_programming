04f7d0d66dfeb856dd9fcc6d9fe1d3b0285fb061
foash-111 <mr2221@fayoum.edu.eg>
2023-11-29 21:37:23 +0200

    my seconde task with python

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(0, 99):
-    if (i < 10):
-        print("{} = 0x{}".format(i, i))
-    else:
-        print("{} = {}".format(i, hex(i)))
+    print("{} = {}".format(i, hex(i)))
