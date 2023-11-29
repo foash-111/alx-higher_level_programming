4e40737b420d9478a37becc8c3bc72328a01e227
foash-111 <mr2221@fayoum.edu.eg>
2023-11-29 22:34:24 +0200

    my seconde task with python

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    output = ""
+    x = 0
     for i in str:
         if (ord(i) >= 97 and ord(i) <= 122):
-            output += chr(ord(i) - 32)
+            x = ord(i) - 32
         else:
-            output += chr(ord(i))
-
-    print(output)
+            x = ord(i)
+        print(chr(x), end="")
+    print("")
