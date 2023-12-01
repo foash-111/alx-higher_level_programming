0a14caae7007150d79ba1dc871931a661b4b6658
foash-111 <mr2221@fayoum.edu.eg>
2023-12-01 11:43:41 +0200

    my third task with python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     if (len(argv) - 1 == 0):
         print("0 arguments.")
     elif (len(argv) - 1 == 1):
-        print("1 argumenent:")
+        print("1 argument:")
         print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(len(argv) - 1))
