a0556eac3b164752b923ab51c3e2df5f5ffa4f83
foash-111 <mr2221@fayoum.edu.eg>
2023-12-02 20:44:35 +0200

    my third task with python

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    arr = sorted(dir(hidden_4))
+    for i in arr:
+        if (i.startswith('__')):
+            pass
+        else:
+            print("{}".format(i))
