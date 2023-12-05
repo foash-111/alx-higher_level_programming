77fa1c3e4e8731be1d44027d46d437b9b0307678
foash-111 <mr2221@fayoum.edu.eg>
2023-12-05 22:20:22 +0200

    my fourth task with python

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-
 def print_reversed_list_integer(my_list=[]):
-    if (my_list == None):
+#if (my_list == None):// erorr in pycode style
+#was wrong in pystyle code because 'is' and '==' aren't the same
+#It's important to note that == checks for equality in value,
+# while is checks for equality in identity 
+# (i.e., both variables point to the exact same object). For example:
+#list1 = [1, 2, 3]
+#list2 = [1, 2, 3]
+#print(list1 == list2)  # Outputs: True
+# print(list1 is list2)  # Outputs: False
+    if (my_list is None):
         return None
     count = -1
     for i in my_list:
