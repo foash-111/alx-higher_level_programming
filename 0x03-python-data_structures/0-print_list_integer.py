41d590a7bcc97a1637b74728f319caf3f9089aba
foash-111 <mr2221@fayoum.edu.eg>
2023-12-05 21:38:41 +0200

    my fourth task with python

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        if my_list is None:
-            return
-        for i in my_list:
-            print("{}".format(i))
+def print_list_integer(my_list=[]):
+    if my_list is None:
+        return
+    for i in my_list:
+        print("{:d}".format(i))
