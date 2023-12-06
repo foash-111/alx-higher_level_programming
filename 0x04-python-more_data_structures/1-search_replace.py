aed87c4f2980e553011708948dbdcce496e2c25a
foash-111 <mr2221@fayoum.edu.eg>
2023-12-06 16:14:26 +0200

    my fifth task with python

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list:
+        new_list = []
+        count = 0
+        for i in my_list:
+            if (i == search):
+                new_list.append(replace)
+            else:
+                new_list.append(i)
+            count += 1
+        return new_list
