f1655c2673ca7c324117e80ff844d3da56558027
foash-111 <mr2221@fayoum.edu.eg>
2023-12-06 02:22:19 +0200

    my fourth task with python

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if (my_list is not None):
+        temp = 0
+        for i in my_list:
+            if (int(i) >= temp):
+                temp = int(i)
+        return (temp)
