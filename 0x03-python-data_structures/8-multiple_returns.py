6ca60c7675c2b538ccccf3d44c53364be67026a9
foash-111 <mr2221@fayoum.edu.eg>
2023-12-06 02:05:19 +0200

    my fourth task with python

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if (sentence is not None):
+    #if (sentence is not None): //if it's = "" will be true and that's wrong
+    if (sentence): # if sentence is  "" or 'None'  will be false
         return (len(sentence), sentence[0])
     else:
         return (0, "")
