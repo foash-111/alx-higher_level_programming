47d9a2011dfbbabd087445b923db5e1b2d2ebce4
foash-111 <mr2221@fayoum.edu.eg>
2023-12-07 09:13:59 +0200

    my fifth task with python

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-        if key:
-            a_dictionary[key] = value
-            return a_dictionary
-        
+    if key:
+        a_dictionary[key] = value
+        return a_dictionary
