ba30c4cfea908d92311274b51320c0a955e8f716
foash-111 <mr2221@fayoum.edu.eg>
2023-12-18 16:46:35 +0200

    exception in python

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,6 +4,7 @@ def safe_print_division(a, b):
     try:
         c = a / b
     except ZeroDivisionError:
+        c = "None"
         return None
     finally:
         print("Inside result:{}".format(c))
