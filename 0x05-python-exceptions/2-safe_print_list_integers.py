a839c61eee142960b1042b6b089f5370c511f1e1
foash-111 <mr2221@fayoum.edu.eg>
2023-12-18 15:03:00 +0200

    exception in python

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/bin/usr/python3
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            counter += 1
+        except IndexError:
+            print("")
+            return counter
+        except TypeError:
+            continue
+        except ValueError:
+            continue
+    print("")
+    return counter
