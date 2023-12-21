4101061b5e2a46b51ae9626d6fd2911be146c6d4
foash-111 <mr2221@fayoum.edu.eg>
2023-12-21 16:43:17 +0200

    classes in python

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+this is a 0-square module that that defines a square class
+"""
+
+
+class Square:
+    """ a class Square that defines a square:"""
+    
+    def __init__(self, size):
+        self.__size = size
