ad48ce685925ba224e8bb24c5052490f8a2c008d
foash-111 <mr2221@fayoum.edu.eg>
2023-12-21 17:03:17 +0200

    classes in python

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+this is a 0-square module that that defines a square class
+"""
+
+
+class Square:
+    """ a class Square that defines a square:"""
+
+    def __init__(self, size=0):       
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
