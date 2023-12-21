df9a43e2b39cdc9024c0c35576511a48ec837cb8
foash-111 <mr2221@fayoum.edu.eg>
2023-12-21 16:44:02 +0200

    classes in python

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,6 +6,6 @@ this is a 0-square module that that defines a square class
 
 class Square:
     """ a class Square that defines a square:"""
-    
+
     def __init__(self, size):
-        self.__size = size
+        self._size = size
