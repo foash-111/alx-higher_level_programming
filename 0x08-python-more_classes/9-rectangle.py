fff07315064dc02b9bf3a6953a8ac18d3652887c
foash-111 <mr2221@fayoum.edu.eg>
2024-01-05 16:07:23 +0200

    hash-table

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -22,21 +22,21 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if isinstance(value, int) and value >= 0:
-            self.__width = value
-        if value < 0:
-            raise ValueError("width must be >= 0")
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        if isinstance(value, int) and value >= 0:
+            self.__width = value
 
     @height.setter
     def height(self, value):
-        if isinstance(value, int) and value >= 0:
-            self.__height = value
-        if value < 0:
-            raise ValueError("height must be >= 0")
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        if isinstance(value, int) and value >= 0:
+            self.__height = value
 
     def area(self):
         return self.width * self.height
