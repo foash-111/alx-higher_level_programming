7b535516851a503d3752ceef0ee4c121e0f10441
foash-111 <mr2221@fayoum.edu.eg>
2024-01-08 01:49:03 +0200

    hash-table

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+print square with #
+
+return nothing
+"""
+
+
+def print_square(size):
+    if (isinstance(size, float) and size < 0) or not\
+            isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
