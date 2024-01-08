51064cedeb40f1b0df6a7717cc519071fd11ee80
foash-111 <mr2221@fayoum.edu.eg>
2024-01-09 00:02:48 +0200

    hash-table

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""
+check if the object is from specific class
+
+return true or false
+"""
+
 
 def is_same_class(obj, a_class):
-    if type(obj) is  a_class:
+    """
+    check if the instance type is from specific class
+    """
+
+    if type(obj) is a_class:
         return True
     else:
         return False
