c0905de14abbf45ea11cec031557c74544aff1d6
foash-111 <mr2221@fayoum.edu.eg>
2024-01-09 03:16:54 +0200

    inheritence in python

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+class MyInt that inherits from int
+
+MyInt is a rebel. MyInt has == and != operators inverted
+"""
+
+
+class MyInt(int):
+
+    def __init__(self, num):
+        self.num = num
+        super().__init__()
+
+    def __str__(self):
+        return "{}".format(self.num)
+
+    def __eq__(self, other):
+        return self.num != other
+
+    def __ne__(self, other):
+        return self.num == other
