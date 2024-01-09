1e3fdb823cd2356f261441c63a0699b6f763a20d
foash-111 <mr2221@fayoum.edu.eg>
2024-01-09 02:17:51 +0200

    inheritence in python

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+BaseGeometry class
+
+return nothing
+"""
+
+
+class BaseGeometry:
+    """area nothing"""
+    def area(self):
+        """raise exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.name = name
+        self.value = value
+        if not isinstance(self.value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if self.value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
