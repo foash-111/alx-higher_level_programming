ec2163381d9646e33bcfcb199f289e56743b49c6
foash-111 <mr2221@fayoum.edu.eg>
2024-01-11 23:53:10 +0200

    Python - Input/Output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,12 +4,12 @@
 
 
 class Student:
-    """define and retrive"""
+    """define and retrive instance as a object"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """retrive instance as a dict"""
+        """retrive any instance from the class as a dict object"""
         return self.__dict__
