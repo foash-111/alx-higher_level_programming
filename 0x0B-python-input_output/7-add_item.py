b95647603e854e80fc6acbde2b8e5bc7a5871228
foash-111 <mr2221@fayoum.edu.eg>
2024-01-11 22:58:57 +0200

    Python - Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,7 +14,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 my_list = load_from_json_file("add_item.json")
-if my_list == None:
+if my_list is None:
     my_list = []
 
 i = 1
