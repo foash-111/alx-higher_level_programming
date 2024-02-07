2176bd02dca7dc048726a92f17a5d02dab5a5095
foash-111 <mr2221@fayoum.edu.eg>
2024-02-08 00:46:16 +0200

    unnitest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,18 +7,23 @@ from max_file import max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_max_integer_empty_list(self):
+        """doc"""
         self.assertIsNone(max_integer([]))
 
     def test_max_integer_single_element(self):
+            """doc"""
         self.assertEqual(max_integer([5]),  5)
 
     def test_max_integer_multiple_elements(self):
+        """doc"""
         self.assertEqual(max_integer([1,  2,  3,  4]),  4)
 
     def test_max_integer_negative_numbers(self):
+        """doc"""
         self.assertEqual(max_integer([-1, -2, -3,  4]),  4)
 
     def test_max_integer_non_integer_values(self):
+        """doc"""
         with self.assertRaises(ValueError):
             max_integer([1, "two",  3])
 
