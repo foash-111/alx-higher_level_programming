cdf243adb66c67b1546069d3daf2d620888b61a9
foash-111 <mr2221@fayoum.edu.eg>
2024-02-08 14:18:17 +0200

    doctest

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -11,8 +11,10 @@ def say_my_name(first_name, last_name=""):
     """
     print full name
     """
-    if not (isinstance(first_name, str) or first_name):
+    if not (isinstance(first_name, str)):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+    if first_name == "" and last_name == "":
+        raise ValueError("provide me with one argument at least")
     print("My name is", first_name, last_name)
