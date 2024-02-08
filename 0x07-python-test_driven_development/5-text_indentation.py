8bf1e01536d6cd4608b4f7ca72d7d0da13fab4eb
foash-111 <mr2221@fayoum.edu.eg>
2024-02-08 14:48:50 +0200

    doctest

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,8 +10,11 @@ def text_indentation(text):
     """print \n\n after . , ?"""
     length = 0
     current = 0
-    for value in (text):
-        print(value, end="")
-        if value in {'.', '?', ':'}:
-            print()
-            print()
+    if type(text) is str:
+        for value in (text):
+            print(value, end="")
+            if value in {'.', '?', ':'}:
+                print()
+                print()
+    else:
+        raise TypeError("text must be a string")
