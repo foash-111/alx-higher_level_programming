7bf08658012aa3c0a9e32ce25873c10de3389bf5
foash-111 <mr2221@fayoum.edu.eg>
2024-02-10 22:49:45 +0200

    square class getter and setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,9 +9,24 @@ class Square(Rectangle):
     """Square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """call rectangle constructor"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """return information about the instance when we print it"""
         return "[Square] ({}) {}/{} - {}"\
             .format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """getter method"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            self.width = value
+        else:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
