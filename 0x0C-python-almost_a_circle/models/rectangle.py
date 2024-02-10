001dbd4c5ce7c0334867c935cfd6d83263d90c18
foash-111 <mr2221@fayoum.edu.eg>
2024-02-10 21:48:15 +0200

    update **kwargs method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -114,8 +114,8 @@ class Rectangle(Base):
         for attr, value in zip(attributes, args):
             """args considered as tuple"""
             setattr(self, attr, value)
-
-        for key, cntt in kwargs.items():
+        if len(args > 0):
             """kwargs considered as dic
             with key=value pair passed to the class when we creat instance"""
-            self.__dict__[key] = cntt
+            for key, cntt in kwargs.items():
+                self.__dict__[key] = cntt
