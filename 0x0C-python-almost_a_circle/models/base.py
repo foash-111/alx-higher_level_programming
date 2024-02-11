3cc6172506717922fa02abf8b0551fb54e450c72
foash-111 <mr2221@fayoum.edu.eg>
2024-02-11 11:01:44 +0200

    add method that returns the string representation of a Rectangle dict

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """base class"""
 
+import json
 
 class Base:
     """my base class"""
@@ -14,3 +15,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    
+    def to_json_string(list_dictionaries):
+        """accept an opject and return string representation of it"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
