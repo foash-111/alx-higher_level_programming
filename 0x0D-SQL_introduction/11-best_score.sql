4f11c252d5a1bdff3f548cd5b76ffdbbbb1c684e
foash-111 <mr2221@fayoum.edu.eg>
2024-02-25 01:26:50 +0200

    diplay ordered by score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 --  a script that lists all records of the table second_table of the database hbtn_0c_0
-SELECT "name", "score" FROM second_table
-ORDER BY "score" DESC
-WHERE "score" >= 10;
+SELECT name, score FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
