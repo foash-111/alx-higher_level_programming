279bda0220d5ced2cc3f3a023d24e171c9993728
foash-111 <mr2221@fayoum.edu.eg>
2024-03-07 16:53:34 +0200

    select from mpre than one table

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 --  a script that lists all cities contained in the database hbtn_0d_usa.
 
-SELECT id, name, states.name  FROM cities
+SELECT cities.id, cities.name, states.name  FROM cities
 JOIN states ON states.id = cities.id
 ORDER BY id ASC;
