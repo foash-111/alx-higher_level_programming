8a7d1bebd66113526c936285919cddff9c2ddd01
foash-111 <mr2221@fayoum.edu.eg>
2024-03-15 13:58:24 +0200

    if else with while loop

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,15 +3,11 @@
 const args = process.argv;
 let i = 0;
 let j = 0;
-if (args[2] !== undefined) {
-  if (isNaN(args[2]) === false) {
-      while (i < args[2]) {
-          console.log('C is fun');
-          i++;
-      }
-  } else {
-      console.log('Missing number of occurrences');
-    }    
+if ((args[2] === undefined) || (isNaN(args[2]) === true)) {
+  console.log('Missing number of occurrences');   
 } else {
-  console.log('Missing number of occurrences');
+  while (i < args[2]) {
+    console.log('C is fun');
+    i++;
+}
 }
