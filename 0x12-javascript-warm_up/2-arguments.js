073c2fe8728d8c7f303391c5b7c38a8c10ccde9d
foash-111 <mr2221@fayoum.edu.eg>
2024-03-15 13:08:38 +0200

    print using console.log(...)

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
 const args = process.argv;
 
 if (args[2] !== undefined) {
-  console.log('Argument found');
+  console.log('Arguments found');
 } else {
   /* console.log(args[0]);
   console.log(args[1]); */
