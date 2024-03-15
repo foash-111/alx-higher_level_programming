5f3480e2b56d0f98c9a43553e47b429d85ed5b91
foash-111 <mr2221@fayoum.edu.eg>
2024-03-15 13:15:23 +0200

    print using console.log(...)

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -5,12 +5,10 @@ const args = process.argv;
 if (args[2] !== undefined) {
   if (args[3] !== undefined){
     console.log(args[2] + ' is ' + args[3])
-  }
-  else {
+  } else {
     console.log(args[2] + ' is undefined')
   }
-}
-else {
+} else {
   /* console.log(args[0]);
   console.log(args[1]); */
   console.log('undefined is undefined');
