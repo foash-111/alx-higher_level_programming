7c1d97d8d0717e5e20834da63652fbb146e0b02a
foash-111 <mr2221@fayoum.edu.eg>
2024-03-15 15:19:41 +0200

    A function that add 2 args from terminal

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+function add(a, b) {
+  return (a * b)
+}
+
+if (isNaN(args[2]) === true || isNaN(args[3] === true)) {
+  console.log('NaN');
+} else {
+  console.log(add(args[2], args[3]))
+}
