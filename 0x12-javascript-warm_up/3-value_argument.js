e8355099c19909e614887b29b6c035bacc47559c
foash-111 <mr2221@fayoum.edu.eg>
2024-03-15 13:14:18 +0200

    print using console.log(...)

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,8 +4,7 @@ const args = process.argv;
 
 if (args[2] !== undefined) {
   console.log(args[2]);
-}
-else {
+} else {
   /* console.log(args[0]);
   console.log(args[1]); */
   console.log('No argument');
