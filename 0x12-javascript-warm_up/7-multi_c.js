ff326d48c00df77b09f6df674c6bb2bf08b3f34c
foash-111 <mr2221@fayoum.edu.eg>
2024-03-15 13:49:56 +0200

    if else

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,18 +3,16 @@
 const args = process.argv;
 let i = 0;
 let j = 0;
-if (args[2] !== undefined){
-  if (args[2] > 0){
-      while (i < args[2]){
+if (args[2] !== undefined) {
+  if (isNaN(args[2]) === false) {
+      while (i < args[2] ){
           console.log('C is fun');
           i++;
       }
-  }
-  else{
+  } else {
       console.log('Missing number of occurrences');
     }
       
-}
-else{
+} else {
   console.log('Missing number of occurrences');
 }
