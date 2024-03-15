1b304c3989589a72ff9e042decbe31397d610a3a
foash-111 <mr2221@fayoum.edu.eg>
2024-03-15 13:17:56 +0200

    print using console.log(...)

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,9 +4,9 @@ const args = process.argv;
 
 if (args[2] !== undefined) {
   if (args[3] !== undefined) {
-    console.log(args[2] + ' is ' + args[3])
+    console.log(args[2] + ' is ' + args[3]);
   } else {
-    console.log(args[2] + ' is undefined')
+    console.log(args[2] + ' is undefined');
   }
 } else {
   /* console.log(args[0]);
