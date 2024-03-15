b8e75d1a6123f6913d53da30d3adee3948dc65ed
foash-111 <mr2221@fayoum.edu.eg>
2024-03-15 14:12:14 +0200

    print square of X with if else and while loop

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,19 +2,17 @@
 
 const args = process.argv;
 let i = 0;
-let j = 0;
-if (args[2] !== undefined){
-  if (args[2] > 0){
-      while (i < args[2]){
-            console.log('X' * args[2]);
-          i++;
-      }
-  }
-  else{
-      console.log('Missing size');
-    }
-      
-}
-else{
+let arr = '';
+if ((args[2] === undefined) || (isNaN(args[2]) === true)) {
   console.log('Missing size');
+} else {
+  while (i < args[2]) {
+    arr += 'X';
+    i++;
+  }
+  i = 0;
+  while (i < args[2]) {
+    console.log(arr);
+    i++;
+  }
 }
