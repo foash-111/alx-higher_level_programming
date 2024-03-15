e536d63faec844bbf18e56e8675d13885a03127c
foash-111 <mr2221@fayoum.edu.eg>
2024-03-15 13:43:57 +0200

    if else

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,8 +3,8 @@
 const args = process.argv;
 
 if (args[2] !== undefined) {
-  if (args[2] >= 0 ) {
-    console.log('My number: ' + args[2]);
+  if (isNaN(args[2]) === false) {
+    console.log('My number: ' + (args[2] | 0)); // Math.floor(args[2])
   } else {
     console.log('Not a number');
   }
