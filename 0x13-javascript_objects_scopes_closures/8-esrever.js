6f0a808f55f365a75cdedbcd042a06fdedd5ee8c
foash-111 <mr2221@fayoum.edu.eg>
2024-03-21 09:46:24 +0200

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   let myList = [];
-  let i =0;
-  let j =0;
+  let i = 0;
+  let j = 0;
 
   while (list[i] !== undefined) {
     i++;
   }
-  
+
   while (i--) {
     myList[j] = list[i];
     j++;
