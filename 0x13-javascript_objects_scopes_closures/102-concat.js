2ee20cdfc671f69f0b3a52c5868fe86755a2ccdb
foash-111 <mr2221@fayoum.edu.eg>
2024-03-21 12:05:31 +0200

    read from file and write in anther file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -8,17 +8,17 @@ fs.readFile(args[2], 'utf8', (error, data) => {
     console.log('error in reading the file');
   } else {
     fs.appendFile(args[4], data, (err) => {
-    if (err) throw err;
-  });
+      if (err) throw err;
+    });
   }
 });
-    
+
 fs.readFile(args[3], 'utf8', (error, data) => {
   if (error) {
     console.log('error in reading the file');
-    } else {
-      fs.appendFile(args[4], data, (err) => {
-      if (err) throw err;
+  } else {
+    fs.appendFile(args[4], data, (err) => {
+    if (err) throw err;
     });
-    }
+  }
 });
