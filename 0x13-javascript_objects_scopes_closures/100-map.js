895984f80c34e0ee551d964414241368aba88f6e
foash-111 <mr2221@fayoum.edu.eg>
2024-03-21 11:02:02 +0200

    use map itarator, add semicolon after return even in curly braces

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,7 +3,7 @@
 const mylist = require('./100-data').list;
 
 console.log(mylist);
-console.log(mylist.map((item, index) => { return item * index }));
+console.log(mylist.map((item, index) => { return item * index; }));
 // map here is an itarator that itarates over the list, and the
 // first argument is first item of the list, second argument is its index and
 // the right side is the oparation that will be done on each item of the list
