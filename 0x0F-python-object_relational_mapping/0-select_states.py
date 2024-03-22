ebc12bd7db06afe12850a2e94ce670d6a4a90eb6
foash-111 <mr2221@fayoum.edu.eg>
2024-03-22 09:18:04 +0200

    add comment

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,28 +3,24 @@
 """my module that link MySQLdb with python"""
 
 
-class Linker:
-  """link MySQLdb with python"""
-  
-  def __init__():
-    import MySQLdb
-    import sys
-    args = sys.argv
-    conn = MySQLdb.connect(host="localhost", port=3306, user="{}".format(args[1]), passwd="{}".format(args[2]), db="{}".format(args[3]), charset="utf8")
+import MySQLdb
+import sys
 
-    cur = conn.cursor()
-    retrive = """
-    SELECT * FROM states
-    ORDER BY id ASC;
-    """
-    cur.execute(retrive)
-
-    states = cur.fetchall()
-
-    for state in states:
-      print(state)
-
-    cur.close()
-    conn.close()
 if __name__ == "__main__":
-  obj = Linker()
+  args = sys.argv
+  conn = MySQLdb.connect(host="localhost", port=3306, user="{}".format(args[1]), passwd="{}".format(args[2]), db="{}".format(args[3]), charset="utf8")
+
+  cur = conn.cursor()
+  retrive = """
+  SELECT * FROM states
+  ORDER BY id ASC;
+  """
+  cur.execute(retrive)
+
+  states = cur.fetchall()
+
+  for state in states:
+    print(state)
+
+  cur.close()
+  conn.close()
