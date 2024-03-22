91968428f6175c580074821bd7f6b7cafaf0c11b
foash-111 <mr2221@fayoum.edu.eg>
2024-03-22 16:29:13 +0200

    anther query use 'LIKE' '%'

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,7 +2,7 @@
 """A module that link MySQLdb with python3"""
 
 
-if __name__ == "__main__":   
+if __name__ == "__main__":
     import MySQLdb
     import sys
     args = sys.argv
@@ -10,24 +10,25 @@ if __name__ == "__main__":
         host="localhost",
         port=3306,
         user="{}".format(args[1]),
-        passwd="{}".format(args[2]),
-        db="{}".format(args[3]),
+        password="{}".format(args[2]),
+        database="{}".format(args[3]),
         charset="utf8"
     )
     data_base_delivery = connection_with_db.cursor()
 
     my_select_query = """
-            SELECT * FROM states
-            WHERE name LIKE 'n%'
+            SELECT id, name FROM states
+            WHERE name LIKE 'N%'
             ORDER BY id ASC;
         """
 
     data_base_delivery.execute(my_select_query)
+
     my_rows = data_base_delivery.fetchall()
 
     for row in my_rows:
         if row[1][0] == "N":
             print(row)
-    
+
     data_base_delivery.close()
     connection_with_db.close()
