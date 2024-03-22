544e658c01896fc197da666a18c1115cee2a1fa3
foash-111 <mr2221@fayoum.edu.eg>
2024-03-22 09:08:14 +0200

    add comment

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,33 +13,10 @@ class Linker:
     conn = MySQLdb.connect(host="localhost", port=3306, user="{}".format(args[1]), passwd="{}".format(args[2]), db="{}".format(args[3]), charset="utf8")
 
     cur = conn.cursor()
-
-    create_table = """
-    CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
-    USE hbtn_0e_0_usa;
-    CREATE TABLE IF NOT EXISTS states ( 
-        id INT NOT NULL AUTO_INCREMENT, 
-        name VARCHAR(256) NOT NULL,
-        PRIMARY KEY (id)
-    );
-    """
-
-    insert_into_table = """
-    INSERT INTO states (name)
-    VALUES
-    ("California"),
-    ("Arizona"),
-    ("Texas"),
-    ("New York"),
-    ("Nevada");
-    """
-
     retrive = """
     SELECT * FROM states
     ORDER BY id ASC;
     """
-    cur.execute(create_table)
-    cur.execute(insert_into_table)
     cur.execute(retrive)
 
     states = cur.fetchall()
