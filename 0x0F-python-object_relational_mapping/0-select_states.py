167c77de7250c06cc887e5334147914808592d23
foash-111 <mr2221@fayoum.edu.eg>
2024-03-22 15:33:02 +0200

    add comment to module, and move the code under if __name__ == '__main__' to not run when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,20 +7,25 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-  args = sys.argv
-  conn = MySQLdb.connect(host="localhost", port=3306, user="{}".format(args[1]), passwd="{}".format(args[2]), db="{}".format(args[3]), charset="utf8")
+    args = sys.argv
+    conn = MySQLdb.connect(
+        host="localhost", port=3306,
+        user="{}".format(args[1]),
+        passwd="{}".format(args[2]),
+        db="{}".format(args[3]),
+        charset="utf8")
 
-  cur = conn.cursor()
-  retrive = """
-  SELECT * FROM states
-  ORDER BY id ASC;
-  """
-  cur.execute(retrive)
+    cur = conn.cursor()
+    retrive = """
+    SELECT * FROM states
+    ORDER BY id ASC;
+    """
+    cur.execute(retrive)
 
-  states = cur.fetchall()
+    states = cur.fetchall()
 
-  for state in states:
-    print(state)
+    for state in states:
+        print(state)
 
-  cur.close()
-  conn.close()
+    cur.close()
+    conn.close()
