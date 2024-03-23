f742b95630ed3085ef4da0ed01d8e4630c0aae54
foash-111 <mr2221@fayoum.edu.eg>
2024-03-23 09:19:21 +0200

    select query with join

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""A module that link MySQLdb with python3"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user="{}".format(args[1]),
+        password="{}".format(args[2]),
+        database="{}".format(args[3]),
+        charset="utf8"
+    )
+    cursor = connection.cursor()
+
+    if ';' in args[4]:
+        raise MySQLdb.Error
+
+    my_select_query = """
+            SELECT cities.name FROM cities
+            JOIN states ON states.id = cities.state_id
+            WHERE states.name = '{}';
+        """.format(args[4])
+
+    cursor.execute(my_select_query)
+
+    my_rows = cursor.fetchall()
+
+    for row in my_rows:
+        print(row)
+
+    cursor.close()
+    connection.close()
