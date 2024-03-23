470bad09d1c74ae6a6e33eeb9c276273ad93dfbe
foash-111 <mr2221@fayoum.edu.eg>
2024-03-23 08:52:56 +0200

    select query

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A module that link MySQLdb with python3"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user="{}".format(args[1]),
+        password="{}".format(args[2]),
+        database="{}".format(args[3]),
+        charset="utf8"
+    )
+    cursor = connection.cursor()
+
+    my_select_query = """
+            SELECT id, name FROM cities
+            ORDER BY id ASC;
+        """
+
+    cursor.execute(my_select_query)
+
+    my_rows = cursor.fetchall()
+
+    for row in my_rows:
+        print(row)
+
+    cursor.close()
+    connection.close()
