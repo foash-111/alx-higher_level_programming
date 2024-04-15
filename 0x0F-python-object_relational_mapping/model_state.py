1f05da246e428cb32dc8407bca52b5d2d02c4000
foash-111 <mr2221@fayoum.edu.eg>
2024-04-15 07:04:09 +0200

    first step to orm using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Sequence
+import sys
+
+args = sys.argv
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(args[1], args[2], args[3]), pool_pre_ping = True)
+
+Base = declarative_base()
+
+class State(Base):
+    """As a table in mysql and contains id, name """
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement = True, primary_key = True)
+    name = Column(String(128), nullable=False)
+
+Base.metadata.create_all(engine)
