2b937eba86e8d6d798205382fb52151d7d8c6531
foash-111 <mr2221@fayoum.edu.eg>
2024-04-15 07:44:36 +0200

    first step to orm using sqlalchemy ooof

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 
 """
-This module demonstrates the first step in using SQLAlchemy for ORM (Object-Relational Mapping).
-It defines a simple SQLAlchemy model for a 'states' table and sets up the database connection.
+This module demonstrates the first step in using SQLAlchemy
+ for ORM (Object-Relational Mapping).
+It defines a simple SQLAlchemy model
+for a 'states' table and sets up the database connection.
 
 Usage:
     python3 6-model_state.py <username> <password> <database_name>
 
-This script creates a 'states' table in the specified MySQL database with two columns:
+This script creates a 'states' table in the specified MySQL database
+ with two columns:
 - 'id': An auto-incrementing integer serving as the primary key.
 - 'name': A string column that cannot be null.
 
-The script uses SQLAlchemy's declarative base system to define the 'State' model and
+The script uses SQLAlchemy's declarative base system to
+define the 'State' model and
 creates the table in the database.
 
 Requirements:
@@ -24,10 +28,12 @@ Date: 2024-04-15
 """
 
 
-from sqlalchemy import  Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
+
 
 class State(Base):
     """
