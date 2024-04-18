becac271a0b9085932564d83554ed2e4edd1c9dc
foash-111 <mr2221@fayoum.edu.eg>
2024-04-18 16:43:26 +0200

    build a relationship and quering with join

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -6,7 +6,7 @@ import class State and city and do quering with join on them
 
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from relationship_state import Base, State
 from relationship_city import City
 from sys import argv
@@ -19,14 +19,12 @@ if __name__ == "__main__":
         .format(argv[1], argv[2], argv[3]), pool_pre_ping=True
     )
     Base.metadata.create_all(engine)
-    _Session = sessionmaker(bind=engine)
-    session = _Session()
-
-    states = session.query(State).order_by(State.id).all()
-
-    for my_state in states:
-        print( "{}: {}".format(my_state.id, my_state.name))
-        for city in my_state.cities:
+    session = Session(engine)
+    # get all states with related city (power of relationship)
+    data = session.query(State).order_by(State.id).all()
+    for state in data:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
             print("\t{}: {}".format(city.id, city.name))
-
+    # ends my connection
     session.close()
