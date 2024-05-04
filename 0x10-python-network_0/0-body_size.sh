c397334a4b8592314f9164fca5b9dd4f785d2b7e
foash-111 <mr2221@fayoum.edu.eg>
2024-05-05 01:14:35 +0300

    use curl

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+
+size=$(curl -I $1 | grep -w 'Content-Length:' | awk '{print $2}')
+echo $size
