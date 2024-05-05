33af5daa8fc1f932c94b540c870c5b27ad65cab5
foash-111 <mr2221@fayoum.edu.eg>
2024-05-05 18:27:17 +0300

    use curl

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a request to that URL, and displays the body of the response
-echo $(curl "$1")
+curl -s -o /dev/null -w "%{http_code}\n" "$1" | grep -q 200 && curl -s "$1"
