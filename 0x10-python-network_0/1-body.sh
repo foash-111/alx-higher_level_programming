88bd154c78b1bc743932c4ea92db60d4c5dabb29
foash-111 <mr2221@fayoum.edu.eg>
2024-05-05 17:46:45 +0300

    use curl

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a request to that URL, and displays the body of the response
-if [ $(curl -I "$1" | grep 'HTTP' | awk '{print$2}') == 200 ]; then echo $(curl "$1"); fi
+echo $(curl "$1")
