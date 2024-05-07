b12b3f018685cf0b08364f4c37b47ecef636436c
foash-111 <mr2221@fayoum.edu.eg>
2024-05-07 17:49:57 +0300

    displays all HTTP methods the server will accept.

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sX OPTIONS -I "$1" | grep "Allow:" | awk '{print substring($0, index($0, "Allow:") + lenght("Allow: "))}'
+curl -sX OPTIONS -I "$1" | grep "Allow: " | awk -F 'Allow: ' '{print $2}'
