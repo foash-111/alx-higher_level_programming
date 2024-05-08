58152144d98ed644dc219a6b899fad23026a947a
foash-111 <mr2221@fayoum.edu.eg>
2024-05-08 13:06:39 +0300

    post with json data

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a Bash script must send a POST request with the contents of a file, passed with the filename as the second argument of the script.
-curl -s -X POST "@$2" $1
+curl -s -H "Content-Type: application/json" -d "@$2" $1
