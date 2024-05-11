7b187cd26298301e02c740953594f933447a93f2
foash-111 <mr2221@fayoum.edu.eg>
2024-05-11 19:42:48 +0300

    network_1 make requests and response with python

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,10 +10,11 @@ if __name__ == "__main__":
     # data as a dict
     data = {'email': sys.argv[2]}
     # encoding data <like a serialize>
-    data = urllib.parse.urlencode(data)
-    # make a request with data and '?' as a swparator between url and query,
-    # name=value&name=value in url after ? called query
-    req = urllib.request.Request(sys.argv[1] + '?' + data)
+    data = urllib.parse.urlencode(data).encode('utf-8')
+    # print(type(data)) // <class 'bytes'>
+    #  data = urllib.parse.urlencode(data)
+    # print(type(data)) // <class 'str'>
+    req = urllib.request.Request(sys.argv[1], data)
     try:
         with urllib.request.urlopen(req) as respons:
             page = respons.read().decode('utf-8')
