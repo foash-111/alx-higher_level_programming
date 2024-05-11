6bc2852f3cf1c34862ea543c04f7eb70d0f40741
foash-111 <mr2221@fayoum.edu.eg>
2024-05-11 19:13:30 +0300

    network_1 make requests and response with python

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         print("    - type:", type(page))
         print("    - content:", page)
         print('    - utf8 content: OK')
-        print(type(respons.headers))
+        # print(type(respons.headers))
     except urllib.error.URLError as e:
         if hasattr(e, 'reason'):
             print(e.reason)
