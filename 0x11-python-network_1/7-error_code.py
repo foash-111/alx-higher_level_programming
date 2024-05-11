754b86587ca9c17487c8e769cc7dc0bb7fb76d9a
foash-111 <mr2221@fayoum.edu.eg>
2024-05-11 22:49:14 +0300

    make a request and get response and  print a specific  header

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,4 +9,6 @@ if __name__ == "__main__":
     response = requests.get(sys.argv[1])
     if response.status_code >= 400:
         print("Error code:", response.status_code)
+    else:
+        print('Index')
     # print(type(respons.headers))
