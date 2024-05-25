e633f5d85e3ac7f86a8525c158532ade1b7e2b19
foash-111 <mr2221@fayoum.edu.eg>
2024-05-25 20:04:19 +0300

    group same values together

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request('https://jsonplaceholder.typicode.com/todos', (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const data = JSON.parse(body);
+  // console.log(data);
+
+  let i = 0;
+  const info = {};
+  let stringUserId = '';
+  let count = 0;
+
+  while (data[i] !== undefined) {
+    if (data[i].completed === true) {
+      count++;
+      stringUserId = data[i].userId.toString();
+      info[stringUserId] = count;
+
+      if (data[i + 1] && data[i + 1].userId !== data[i].userId) {
+        count = 0;
+      }
+    }
+    i++;
+  }
+
+  console.log(info);
+});
