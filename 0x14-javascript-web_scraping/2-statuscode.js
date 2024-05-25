d669b697c678dcba2e37e175f1e50ac7c763e773
foash-111 <mr2221@fayoum.edu.eg>
2024-05-25 17:43:29 +0300

    make a get request using request module

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+// const fetch = require('node-fetch');
+
+// To this
+// import fetch from 'node-fetch';
+
+// const args = process.argv;
+
+// fetch(args[2])
+//   .then(response => {
+//     console.log(response.status);
+//   })
+//   .catch(err => console.log(err));
+const request = require('request');
+
+const args = process.argv;
+
+request(args[2], (err, response) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(response.statusCode);
+});
