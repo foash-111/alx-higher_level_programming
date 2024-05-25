8625eaea706366f55c63e80ec304b9e1a899e2fe
foash-111 <mr2221@fayoum.edu.eg>
2024-05-25 22:47:46 +0300

    get response, then enter titles then enter charactes

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -10,11 +10,11 @@ request('https://swapi-api.alx-tools.com/api/films/', (err, response, body) => {
   const data = JSON.parse(body);
   // console.log(data);
 
-  let i = 0;
+  let i = parseInt(process.argv[2]);
 
-  while (data.results[i] !== undefined) {
-    if (data.results[i].episode_id === parseInt(process.argv[2])) {
-      let j = 0;
+//   while (data.results[i] !== undefined) {
+//     if (data.results[i].episode_id === parseInt(process.argv[2])) {
+    let j = 0;
       while (data.results[i].characters[j] !== undefined) {
         request(data.results[i].characters[j], (err, response, body) => {
           if (err) {
@@ -27,9 +27,9 @@ request('https://swapi-api.alx-tools.com/api/films/', (err, response, body) => {
 
         j = j + 1;
       }
-      break;
-    }
 
-    i = i + 1;
-  }
+    
+
+//     i = i + 1;
+//  }
 });
