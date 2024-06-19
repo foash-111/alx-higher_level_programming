d967b79fc5bae60f5d56860f5aa5f22e1579c602
foash-111 <mr2221@fayoum.edu.eg>
2024-06-19 14:14:44 +0300

    put in your consider x, y axis

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,8 +88,11 @@ class Rectangle(Base):
 
     def display(self):
         """display rectangle using #"""
+        for i in range(self.__y):
+            print()
+
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """overriding the __str__ method"""
