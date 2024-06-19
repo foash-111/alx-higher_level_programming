a3d75fd1db05a81f7ab4750b8bb652a1d8eaf1bf
foash-111 <mr2221@fayoum.edu.eg>
2024-06-19 11:50:30 +0300

    Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,40 +1,17 @@
 #!/usr/bin/python3
 """"my rectangle class"""
 
+
 from models.base import Base
 
 
 class Rectangle(Base):
     """Rectangle class"""
-
     def __init__(self, width, height, x=0, y=0, id=None):
-        if type(width) is int:
-            self.__width = width
-        else:
-            raise TypeError("width must be an integer")
-        if width <= 0:
-            raise ValueError("width must be > 0")
-
-        if type(height) is int:
-            self.__height = height
-        else:
-            raise TypeError("height must be an integer")
-        if height <= 0:
-            raise ValueError("height must be > 0")
-
-        if type(x) is int:
-            self.__x = x
-        else:
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-
-        if type(y) is int:
-            self.__y = y
-        else:
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         Base.__init__(self, id)
 
     @property
@@ -43,12 +20,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if type(value) is int:
-            self.__width = value
-        else:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -56,74 +28,4 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if type(value) is int:
-            self.__height = value
-        else:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-
-    @property
-    def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        if type(value) is int:
-            self.__x = value
-        else:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-
-    @property
-    def y(self):
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        if type(value) is int:
-            self.__y = value
-        else:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-
-    def area(self):
-        """get rectangle area"""
-        return self.__width * self.__height
-
-    def display(self):
-        """display rectangle with #"""
-        for j in range(self.__y):
-            print()
-        for i in range(self.__height):
-            print(" " * self.__x + "#" * self.__width)
-
-    def __str__(self):
-        """Update the class Rectangle by overriding the __str__ method
-        return information about the instance when we print it"""
-        return "[Rectangle] ({}) {}/{} - {}/{}"\
-            .format(self.id, self.__x, self.__y, self.__width, self.__height)
-
-    def update(self, *args, **kwargs):
-        """update my arguments in order considered
-        the number of arguments passed"""
-        attributes = ("id", "width", "height", "x", "y")
-
-        if args:
-            for attr, value in zip(attributes, args):
-                """args considered as tuple"""
-                """setattr: set attribute <- value  of its instance
-                and creat attribute if it doesn't exist"""
-                setattr(self, attr, value)
-        else:
-            """kwargs considered as dic
-            with key=value pair passed to the class when we creat instance"""
-            for key, content in kwargs.items():
-                setattr(self, key, content)
-
-    def to_dictionary(self):
-        """returns the dictionary representation of a Rectangle"""
-        return {"x": self.x, "y": self.y, "id": self.id,
-                "height": self.height, "width": self.width}
+        self.__height = value
