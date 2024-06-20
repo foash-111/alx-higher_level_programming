50ac44a46705c2d2d20366bcb56bee3905fa7e01
foash-111 <mr2221@fayoum.edu.eg>
2024-06-20 14:13:18 +0300

    addiing to_dictionary(self): *args and **kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -43,3 +43,8 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """retrun dict representation"""
+        return {"id": self.id, "size": self.width,
+                "x": self.x, "y": self.y}
