1a9cc5dbc8c451f7e21c77eb8b53bff1a19896e4
foash-111 <mr2221@fayoum.edu.eg>
2024-06-20 10:07:18 +0300

    class Square inherit from rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,46 +4,27 @@
 
 from models.rectangle import Rectangle
 
-
 class Square(Rectangle):
     """Square class"""
-
     def __init__(self, size, x=0, y=0, id=None):
-        """call rectangle constructor"""
+        if type(size) is not int:
+            raise TypeError('width must be an integer')
+        if self.size <= 0:
+            raise ValueError("width must be > 0")
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """return information about the instance when we print it"""
-        return "[Square] ({}) {}/{} - {}"\
+        return"[Square] ({}) {}/{} - {}"\
             .format(self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        """getter method"""
-        return self.width
-
+        return self.size
+    
     @size.setter
     def size(self, value):
-        if type(value) is int:
-            self.width = value
-        else:
-            raise TypeError("width must be an integer")
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
         if value <= 0:
             raise ValueError("width must be > 0")
-
-    def update(self, *args, **kwargs):
-        """update args--> tuple"""
-        """update kwargs --> dictionary if args doesn't found"""
-
-        if args:
-            attributes = ("id", "size", "x", "y")
-            for key, value in zip(attributes, args):
-                setattr(self, key, value)
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-    def to_dictionary(self):
-        """returns the dictionary representation of a Square"""
-        return {"id": self.id, "x": self.x, "size": self.width,
-                "y": self.y}
+        self.size = value
