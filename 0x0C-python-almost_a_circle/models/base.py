c09e9635d9fa9f6204102d76d9e5b44ba7d490e9
foash-111 <mr2221@fayoum.edu.eg>
2024-06-20 15:52:14 +0300

    convert list items into dictonary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """base class"""
 
-from json import dumps
+from json import dumps, dump
 
 
 class Base:
@@ -23,3 +23,25 @@ class Base:
             return dumps(list_dictionaries)
         else:
             return "[]"
+
+    @staticmethod
+    def to_dictionary(self):
+        """retrun dict representation"""
+        return {"id": self.id, "width": self.width,
+                "height": self.height, "x": self.x, "y": self.y}
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """itrate over list, convert to dict, append and dump into a file"""
+        my_list = []
+        for i in list_objs:
+            my_dict = i.to_dictionary()
+            my_list.append(my_dict)
+
+        my_string = cls.to_json_string(my_list)
+
+        with open("{}.json".format(cls.__name__), "w") as file:
+            if list_objs:
+                dump(my_string, file)
+            else:
+                dump("[]", file)
