680576544102255d65d581867c30b9841a8d6773
foash-111 <mr2221@fayoum.edu.eg>
2024-06-20 15:58:51 +0300

    convert list items into dictonary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,18 +17,18 @@ class Base:
             self.id = Base.__nb_objects
 
     @staticmethod
+    def to_dictionary(self):
+        """retrun dict representation"""
+        return {"id": self.id, "width": self.width,
+                "height": self.height, "x": self.x, "y": self.y}
+
+    @staticmethod
     def to_json_string(list_dictionaries):
         """convert list into json string representation"""
         if list_dictionaries:
             return dumps(list_dictionaries)
         else:
             return "[]"
-
-    @staticmethod
-    def to_dictionary(self):
-        """retrun dict representation"""
-        return {"id": self.id, "width": self.width,
-                "height": self.height, "x": self.x, "y": self.y}
 
     @classmethod
     def save_to_file(cls, list_objs):
@@ -38,9 +38,5 @@ class Base:
             my_dict = i.to_dictionary()
             my_list.append(my_dict)
 
-
         with open("{}.json".format(cls.__name__), "w") as file:
-            if list_objs:
-                dump(my_list, file)
-            else:
-                dump("[]", file)
+            file.write(cls.to_json_string(my_list))
