d70345d3051b8cfaec735d19eea904585c70171f
foash-111 <mr2221@fayoum.edu.eg>
2024-06-20 10:35:07 +0300

    class Square inherit from rectangle, edit __str__, update getter and setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,11 +17,11 @@ class Square(Rectangle):
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}"\
-            .format(self.id, self.x, self.y, self.width)
+            .format(self.id, self.x, self.y, self.size)
 
     @property
     def size(self):
-        return self.size
+        return self.width
 
     @size.setter
     def size(self, value):
@@ -29,4 +29,6 @@ class Square(Rectangle):
             raise TypeError('width must be an integer')
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.size = value
+       
+        self.width = value
+        self.height = value
